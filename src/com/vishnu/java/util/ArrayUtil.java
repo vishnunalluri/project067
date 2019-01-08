4e49d95931386fc4ae7f0de39eb75c27b001c9ab
vishnu.nalluri67@gmail.com <vishnu.nalluri67@gmail.com>
2019-01-09 01:25:41 +0530

    First commit

---
+++ src/com/vishnu/java/util/ArrayUtil.java
@@ -0,0 +1,12 @@
+package com.vishnu.java.util;
+
+public class ArrayUtil {
+
+    public static void  printObjectArray(Object[] object)
+    {
+        for(Object obj : object)
+        {
+            System.out.println(obj);
+        }
+    }
+}
